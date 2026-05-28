342a6934320106988c870a41d279da87f156bada
Maria Eduarda Tenorio Turquete <rm99430@estudante.fieb.edu.br>
2026-05-28 11:31:53 -0300

    Home e Menu do funcionario em construção e criado o sistema de rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 
-import './App.css'
+//import './App.css'
+import HomeFuncionario from './pages/HomeFuncionario/HomeFuncionario'
 
-function App() {
+function App  ()  { 
   
 
   return (
-    <>
-      
-    </>
+    
+      <div className='container'> 
+        <HomeFuncionario/>
+      </div>
+    
   )
 }
 
